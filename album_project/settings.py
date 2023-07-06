bb70e6b4de1840601f650a814f0d59583bf70612
flat1ander <thematthewlane@gmail.com>
2023-07-06 10:35:07 -0400

    Minor change to settings file.

--- album_project/settings.py
+++ album_project/settings.py
@@ -132,15 +132,10 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'main_app/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, '/Users/matthewlane/Documents/SEIR-CARINA/unit4/django_mini_project/main_app/static/styles/')
